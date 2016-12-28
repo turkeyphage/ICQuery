c34f1a5da611a7de71c7aaee6a7e313f32deac52
RogerLo <turkeyworld64@gmail.com>
2016-12-29 00:39:42 +0800

    Add Launch Storyboard

--- ICQuery/AppDelegate.swift
+++ ICQuery/AppDelegate.swift
@@ -18,8 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
         
-
-        
+        sleep(2)
         return true
     }
 
